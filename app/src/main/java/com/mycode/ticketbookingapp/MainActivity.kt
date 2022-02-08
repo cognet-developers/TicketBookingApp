8b6a369261365b0ac79a497930a38f056aa0b78e
Priyadharshini0101 <priyadharshinis5102@gmail.com>
2022-02-08 21:15:02 +0530

    Update

---
+++ app/src/main/java/com/mycode/ticketbookingapp/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.mycode.ticketbookingapp
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatCallback
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.NavigationUI.setupWithNavController
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.firebase.auth.FirebaseAuth
+import com.mycode.ticketbookingapp.databinding.ActivityHomepageBinding
+import com.mycode.ticketbookingapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var appBarConfiguration:AppBarConfiguration
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val binding=DataBindingUtil.setContentView<ActivityMainBinding>(this,R.layout.activity_main)
+
+        val navController=this.findNavController(R.id.myNavHostFragment)
+
+        appBarConfiguration= AppBarConfiguration(navController.graph)
+        setupActionBarWithNavController(navController,appBarConfiguration)
+//        setupWithNavController(navController)
+//        NavigationUI.navigateUp(navController)
+
+    }
+
+}
