2938c7de345adbf81ab0d91ac0042d5f1e2db1dd
reshma <reshmaroyal2002@gmail.com>
2022-02-19 19:19:38 +0530

    Setting intent done

---
+++ app/src/main/java/com/mycode/ticketbookingapp/profilefragment/settingsfragment/Feedback.kt
@@ -0,0 +1,24 @@
+package com.mycode.ticketbookingapp.profilefragment.settingsfragment
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.mycode.ticketbookingapp.R
+import com.mycode.ticketbookingapp.databinding.ActivityFeedbackBinding
+
+
+class Feedback : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // Inflate the layout for this fragment
+        val binding=DataBindingUtil.setContentView<ActivityFeedbackBinding>(this,R.layout.activity_feedback)
+        binding.root
+    }
+
+    fun sendfeedback(){
+        Toast.makeText(applicationContext,"Your feedback has been sent successfully",Toast.LENGTH_SHORT).show()
+    }
+
+}
