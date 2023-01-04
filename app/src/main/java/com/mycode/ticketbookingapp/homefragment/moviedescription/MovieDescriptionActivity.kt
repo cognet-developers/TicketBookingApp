f73f6acfe4d7acdfe492112b4e9f4436f8d86730
Priyadharshini0101 <priyadharshinis5102@gmail.com>
2023-01-04 10:47:05 +0530

    Added: Booking Ticket Pages

--- app/src/main/java/com/mycode/ticketbookingapp/homefragment/moviedescription/MovieDescriptionActivity.kt
+++ app/src/main/java/com/mycode/ticketbookingapp/homefragment/moviedescription/MovieDescriptionActivity.kt
@@ -1,11 +1,9 @@
 package com.mycode.ticketbookingapp.homefragment.moviedescription
 
 import android.annotation.SuppressLint
-import android.app.Application
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
@@ -13,6 +11,7 @@ import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.mycode.ticketbookingapp.R
 import com.mycode.ticketbookingapp.databinding.ActivityMovieDescriptionBinding
+import com.mycode.ticketbookingapp.homefragment.bookingticketpage1.BookingTicketPage1Activity
 import com.mycode.ticketbookingapp.homefragment.gridview.GridViewActivity
 import kotlinx.android.synthetic.main.activity_movie_description.*
 
@@ -36,25 +35,31 @@ class MovieDescriptionActivity:AppCompatActivity() {
         id=intent.getStringExtra(GridViewActivity.USER_KEY).toString()
 
         val viewModelFactory = MovieDescriptionViewModelFactory(id)
-        Log.d("id", id)
         val movieDescriptionViewModel =
             ViewModelProvider(this, viewModelFactory).get(MovieDescriptionViewModel::class.java)
-        binding.movieDescriptionViewModel=movieDescriptionViewModel
+        binding.movieDescriptionViewModel = movieDescriptionViewModel
         binding.lifecycleOwner = this
 
         movieDescriptionViewModel.selectedProperty.observe(this, Observer {
-            if(it!=null){
-                binding.loadingSpinnerM.visibility= View.GONE
+            if (it != null) {
+                binding.loadingSpinnerM.visibility = View.GONE
             }
         })
 
-            binding.movieimg.setOnClickListener{
-
-                Log.d("Videokey", "https://www.youtube.com/watch?v="+movieDescriptionViewModel.selectedvid.value)
-                val utubeintent = Intent(Intent.ACTION_VIEW, Uri.parse("https://www.youtube.com/watch?v="+movieDescriptionViewModel.selectedvid.value))
-                startActivity(utubeintent)
+        movieDescriptionViewModel.navigateToBookingTicketPage1.observe(this, Observer {
+            if (it == true) {
+                val intent = Intent(application, BookingTicketPage1Activity::class.java)
+                startActivity(intent)
             }
+        })
 
+        binding.movieimg.setOnClickListener {
+            val utubeintent = Intent(
+                Intent.ACTION_VIEW,
+                Uri.parse("https://www.youtube.com/watch?v=" + movieDescriptionViewModel.selectedvid.value)
+            )
+            startActivity(utubeintent)
+        }
 
 
     }
